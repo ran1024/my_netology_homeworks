94c55574ac902d9ca8901a0187ebb661a18dccee
Aleksei Remnev <ran1024@yandex.ru>
2019-03-08 10:19:30 +1000

    Здесь ещё всё работает.

--- 3_3_classesVK/3_3_classes_vk.py
+++ 3_3_classesVK/3_3_classes_vk.py
@@ -5,48 +5,61 @@
 #
 
 import requests
+import functools
 import pprint
 
-TOKEN = 'b534eb8cb534eb8cb534eb8c80b55df025bb534b534eb8ce94b94b6299c2a63772794e9'
 
 class GetUserVK:
 
-    def __init__(self, access_token):
+    def __init__(self, access_token, user_id):
         self.access_token = access_token
+        self.user_id = user_id
         self.url = 'https://api.vk.com/method/'
-
-    def get_params(self):
-        return {
+        self.params = {
             'access_token': self.access_token,
+            'user_id': user_id,
+            'fields': 'nickname',
             'v': 5.92,
         }
 
-    def get_friends(self, user_id):
-        """
-        Функция получает список друзей, удаляет забаненные и удалённые записи.
-        Возвращает список славарей: {идентификатор: имя_фамилия} друзей пользователя.
-        :param user_id:
-        :return:
-        """
-        params = self.get_params()
-        params['user_id'] = user_id
-        params['fields'] = 'nickname'
-        response = requests.get('{0}friends.get'.format(self.url), params)
-        friend_list = response.json()['response']['items']
-        friends = []
-        while friend_list:
-            item = friend_list.pop()
-            if 'deactivated' not in item:
-               friends.append({item['id']: item['first_name'] + ' ' + item['last_name']})
-        return friends
+
+def get_user_vk(url, command, params):
+    """
+    Функция получает список друзей, удаляет забаненные и удалённые записи.
+    Возвращает множество, содержащее идентификаторы друзей пользователя.
+    :param self:
+    :return:
+    """
+    response = requests.get('{0}{1}.get'.format(url, command), params)
+    friend_list = response.json()['response']['items']
+    friends = set()
+    while friend_list:
+        item = friend_list.pop()
+        if 'deactivated' not in item:
+            friends.add(item['id'])
+    return friends
 
 
-user1 = GetUserVK(TOKEN)
-user1_friends = user1.get_friends('6888361')
-user2 = GetUserVK(TOKEN)
-user2_friends = user1.get_friends('23539')
+def main():
+    users_id = (6888361, 23539, 8781042)
+    url = 'https://api.vk.com/method/'
+    TOKEN = 'b534eb8cb534eb8cb534eb8c80b55df025bb534b534eb8ce94b94b6299c2a63772794e9'
+    users_friend = []
+    params = {'access_token': TOKEN, 'fields': 'nickname', 'v': 5.92}
 
-#user1_friends = user1.get_friends('1945830')
-print(len(user1_friends))
-pprint.pprint(user1_friends)
+    for i in range(len(users_id)):
+        params['user_id'] = users_id[i]
+        users_friend.append(get_user_vk(url, 'friends', params))
+        print(f"User {users_id[i]} имеет {len(users_friend[i])} друзей.")
+
+    friends_id = list(functools.reduce(lambda x, y: x & y, users_friend))
+    if friends_id[0]:
+        print("Число общих друзей:", len(friends_id))
+        pprint.pprint(friends_id)
+    else:
+        print("У этих пользователей нет общих друзей!")
+        exit(1)
+
+
+main()
 
