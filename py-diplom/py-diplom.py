d36fedc40623ddda660231ab70b3f1c7949651cf
Aleksei Remnev <ran1024@yandex.ru>
2019-03-15 11:11:19 +1000

    Провёл оптимизацию кода.

--- py-diplom/py-diplom.py
+++ py-diplom/py-diplom.py
@@ -18,11 +18,38 @@ class UserVK:
         self.url = 'https://api.vk.com/method/'
         self.token = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
         self.user_id = ''
+        self.user_name = ''
         self.user_friends = []
         self.params = {
             'access_token': self.token,
+            'fields': 'first_name',
             'v': 5.92,
         }
+
+    def get_user_by_id(self, user_id):
+        """
+        Функция получает ID пользователя.
+        Возвращает имя пользователя или ошибку, если пользователя не существует.
+        :param id:
+        :return:
+        """
+        self.params['user_ids'] = user_id
+        # self.params['fields'] = 'first_name'
+        response = requests.get('{0}users.get'.format(self.url), self.params)
+        try:
+            # if 'error' in response.json():
+            #     raise KeyError
+            #     return 'Error', 'Такого пользователя не существует! Для следующей попытки нажмите "Старт".'
+            # else:
+            result = response.json()['response'][0]
+            self.user_name = f"{result['first_name']} {result['last_name']}"
+            self.user_id = user_id
+            return 'user_name', self.user_name
+        except KeyError:
+            return 'Error', 'Такого пользователя не существует! Для следующей попытки нажмите "Старт".'
+        finally:
+            del self.params['user_ids']
+            # del self.params['fields']
 
     def get_userid_by_name(self, name):
         """
@@ -44,7 +71,6 @@ class UserVK:
         finally:
             del self.params['screen_name']
 
-
     def get_friends(self, user_id=None):
         """
         Функция получает список друзей, удаляет забаненные и удалённые записи.
@@ -55,7 +81,7 @@ class UserVK:
             self.user_id = user_id
 
         self.params['user_id'] = self.user_id
-        self.params['fields'] = 'first_name'
+        # self.params['fields'] = 'first_name'
         response = requests.get('{0}friends.get'.format(self.url), self.params)
         try:
             friend_list = response.json()['response']['items']
@@ -66,6 +92,8 @@ class UserVK:
             return 'count', response.json()['response']['count']
         except KeyError:
             return 'Error', 'Не удалось получить список друзей. Для следующей попытки нажмите "Старт".'
+        finally:
+            del self.params['user_id']
 
 
 class App(npyscreen.StandardApp):
@@ -91,13 +119,17 @@ class MainForm(npyscreen.ActionForm):
         self.text2 = self.add(npyscreen.FixedText, value='', editable=False)
 
         self.slider = self.add(npyscreen.SliderPercent, out_of=100, step=1, rely=self.y - 4, editable=False)
+        self.user = UserVK()
         self.ind = 0
 
-    def get_user_attr(self):
-        user = UserVK()
-        self.user_name_id.editable = False
-        if self.user_name_id.value.isdigit():
-            result = user.get_friends(self.user_name_id.value)
+    def processing_of_result(self, result, text):
+        if result[0] == 'Error':
+            self.user_name_id.value = ''
+            self.text1.value = result[1]
+        else:
+            self.text1.value = f'{text} пользователя: {result[1]}'
+            self.slider.value = 5
+            result = self.user.get_friends()
             if result[0] == 'Error':
                 self.user_name_id.value = ''
                 self.text2.value = result[1]
@@ -105,22 +137,15 @@ class MainForm(npyscreen.ActionForm):
                 self.text2.value = f'Количество друзей: {result[1]}'
                 self.slider.value = 10
                 self.ind = 1
+
+    def get_user_attr(self):
+        self.user_name_id.editable = False
+        if self.user_name_id.value.isdigit():
+            result = self.user.get_user_by_id(self.user_name_id.value)
+            self.processing_of_result(result, 'Имя')
         else:
-            result = user.get_userid_by_name(self.user_name_id.value)
-            if result[0] == 'Error':
-                self.user_name_id.value = ''
-                self.text1.value = result[1]
-            elif result[0] == 'id':
-                self.text1.value = f'ID пользователя: {result[1]}'
-                self.slider.value = 5
-                result = user.get_friends()
-                if result[0] == 'Error':
-                    self.text2.value = result[1]
-                    self.user_name_id.value = ''
-                else:
-                    self.text2.value = f'Количество друзей: {result[1]}'
-                    self.slider.value = 10
-                    self.ind = 1
+            result = self.user.get_userid_by_name(self.user_name_id.value)
+            self.processing_of_result(result, 'ID')
 
     # переопределенный метод, срабатывающий при нажатии на кнопку «Выход»
     def on_ok(self):
