c4ed11de8e96b0ade7f24211505858094d0d12e0
Aleksei Remnev <ran1024@yandex.ru>
2019-03-15 15:57:07 +1000

    Сделал запрос кол-ва групп пользователя.

--- py-diplom/py-diplom.py
+++ py-diplom/py-diplom.py
@@ -8,9 +8,6 @@ import npyscreen
 from time import sleep
 import requests
 
-TOKEN = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
-url = 'https://api.vk.com/method/'
-
 
 class UserVK:
 
@@ -20,36 +17,31 @@ class UserVK:
         self.user_id = ''
         self.user_name = ''
         self.user_friends = []
+        self.user_groups = set()
         self.params = {
             'access_token': self.token,
             'fields': 'first_name',
             'v': 5.92,
         }
 
-    def get_user_by_id(self, user_id):
+    def get_user_by_id(self, userid):
         """
         Функция получает ID пользователя.
         Возвращает имя пользователя или ошибку, если пользователя не существует.
         :param id:
         :return:
         """
-        self.params['user_ids'] = user_id
-        # self.params['fields'] = 'first_name'
+        self.params['user_ids'] = userid
         response = requests.get('{0}users.get'.format(self.url), self.params)
         try:
-            # if 'error' in response.json():
-            #     raise KeyError
-            #     return 'Error', 'Такого пользователя не существует! Для следующей попытки нажмите "Старт".'
-            # else:
             result = response.json()['response'][0]
             self.user_name = f"{result['first_name']} {result['last_name']}"
-            self.user_id = user_id
+            self.user_id = userid
             return 'user_name', self.user_name
         except KeyError:
             return 'Error', 'Такого пользователя не существует! Для следующей попытки нажмите "Старт".'
         finally:
             del self.params['user_ids']
-            # del self.params['fields']
 
     def get_userid_by_name(self, name):
         """
@@ -65,7 +57,7 @@ class UserVK:
                 self.user_id = result["object_id"]
                 return 'id', self.user_id
             else:
-                return 'Error', 'Это не пользователь а группа или приложение.'
+                return 'Error', 'Это не пользователь а группа или приложение. Для следующей попытки нажмите "Старт".'
         except TypeError:
             return 'Error', 'Такого пользователя не существует! Для следующей попытки нажмите "Старт".'
         finally:
@@ -74,14 +66,15 @@ class UserVK:
     def get_friends(self, user_id=None):
         """
         Функция получает список друзей, удаляет забаненные и удалённые записи.
-        Возвращает множество, содержащее имена и фамилии друзей пользователя.
+        Возвращает количество друзей пользователя. Список, содержащий id друзей
+        пользователя запоминается в атрибуте экземпляра класса.
+        :param user_id:
         :return:
         """
         if user_id is not None:
             self.user_id = user_id
 
         self.params['user_id'] = self.user_id
-        # self.params['fields'] = 'first_name'
         response = requests.get('{0}friends.get'.format(self.url), self.params)
         try:
             friend_list = response.json()['response']['items']
@@ -94,6 +87,25 @@ class UserVK:
             return 'Error', 'Не удалось получить список друзей. Для следующей попытки нажмите "Старт".'
         finally:
             del self.params['user_id']
+
+    def get_groups(self, user_id=None):
+        """
+        Функция возвращает количество групп у пользователя. Множество, содержащее
+        id групп пользователя запоминается в атрибуте экземпляра класса.
+        :param user_id:
+        :return:
+        """
+        if user_id is None:
+            self.params['user_id'] = self.user_id
+        else:
+            self.params['user_id'] = user_id
+        response = requests.get('{0}groups.get'.format(self.url), self.params)
+        try:
+            result = response.json()['response']['items']
+            self.user_groups = set(result)
+            return 'count', response.json()['response']['count']
+        except KeyError:
+            return 'Error', 'Профиль пользователя закрыт или отключён! Для следующей попытки нажмите "Старт".'
 
 
 class App(npyscreen.StandardApp):
@@ -117,6 +129,7 @@ class MainForm(npyscreen.ActionForm):
         self.add(npyscreen.FixedText, value=''.join(['-' for i in range(self.x - 4)]), editable=False)
         self.text1 = self.add(npyscreen.FixedText, value='', editable=False)
         self.text2 = self.add(npyscreen.FixedText, value='', editable=False)
+        self.text3 = self.add(npyscreen.FixedText, value='', editable=False)
 
         self.slider = self.add(npyscreen.SliderPercent, out_of=100, step=1, rely=self.y - 4, editable=False)
         self.user = UserVK()
@@ -137,6 +150,15 @@ class MainForm(npyscreen.ActionForm):
                 self.text2.value = f'Количество друзей: {result[1]}'
                 self.slider.value = 10
                 self.ind = 1
+                result = self.user.get_groups()
+                if result[0] == 'Error':
+                    self.user_name_id.value = ''
+                    self.text3.value = result[1]
+                    self.ind = 0
+                else:
+                    self.text3.value = f'Количество групп: {result[1]}'
+                    self.slider.value = 15
+                    self.ind = 1
 
     def get_user_attr(self):
         self.user_name_id.editable = False
@@ -166,4 +188,3 @@ class MainForm(npyscreen.ActionForm):
 if __name__ == '__main__':
     MyApp = App()
     MyApp.run()
-    # main()
