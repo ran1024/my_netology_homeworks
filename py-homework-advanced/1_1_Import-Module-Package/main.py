d1af67965eefcebfc87f6cd2fa63e678ec9ad306
Aleksei Remnev <ran1024@yandex.ru>
2019-03-28 17:28:37 +1000

    Создана структура приложения.

---
+++ py-homework-advanced/1_1_Import-Module-Package/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# file-name.py
+#
+#  Copyright 2019 Aleksei Remnev <ran1024@yandex.ru>
+#
+
+
+if __name__ == '__main__':
+    import sys
+
+    sys.exit(main(sys.argv))
