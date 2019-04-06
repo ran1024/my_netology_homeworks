974c6dd5eed4bac9a89504131ec92e06b8018dc4
Aleksei Remnev <ran1024@yandex.ru>
2019-04-06 22:13:16 +1000

    Вроде бы сделано.

--- py-homework-advanced/1_4_Function20_args_kwargs/phone_book.py
+++ py-homework-advanced/1_4_Function20_args_kwargs/phone_book.py
@@ -7,6 +7,8 @@
 #
 import re
 
+book_list = []
+
 
 class Contact(object):
 
@@ -45,8 +47,10 @@ class PhoneBook(object):
     def add_contact(self,  name, surname, phone, elite, *args, **kwargs):
         contact = Contact(name, surname, phone, elite, *args, **kwargs)
         self.contact_dict[phone] = contact
+        self.list_contact()
 
     def list_contact(self):
+        print('\n\n{0:-^64}'.format(self.name_phone_book))
         for key in self.contact_dict:
             print(self.contact_dict[key])
 
@@ -54,28 +58,26 @@ class PhoneBook(object):
         if num in self.contact_dict:
             try:
                 self.contact_dict.pop(num)
+                print(f'Контакт с номером {num} удалён.\n')
             except KeyError:
                 print('Ошибка! Запись не найдена.')
         else:
             print(f'Контакта с номером телефона: {num} не существует.')
 
     def search_contact(self, name, surname):
+        print('\n\n{0:-^64}'.format(f'Поиск контакта: {name} {surname}'))
+        contact = False
         for val in self.contact_dict.values():
             if surname == val.surname and name == val.name:
-                print(val)
-
-
-text_commands = f'a -добавить l -вывод всех контактов d -удалить s -поиск q -выход'
-text_first = f'n -новая тлф.книга  "номер" - открыть тлф.книгу  q -Выход'
-title1 = 'Программа Телефонная Книга'
-book_list = []
+                contact = val
+        print(contact) if contact else print('Контакт не найден.')
 
 
 def edit_book(num):
+    # print('\n\n{0:-^64}'.format(book_list[num].name_phone_book))
+    book_list[num].list_contact()
     while True:
-        print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n{0:-^64}'.format(book_list[num].name_phone_book))
-        print(book_list[num].list_contact())
-        print(text_commands)
+        print(f'\na -добавить l -вывод всех контактов d -удалить s -поиск q -выход')
         command = input('\nВведите команду: ')
         if re.search(r'^[aldsq]?$', command) and command:
             if command =='q':
@@ -83,16 +85,16 @@ def edit_book(num):
             elif command == 'a':
                 name = input('\nВведите имя: ')
                 if name:
-                    surname = input('\nВведите фамилию: ')
+                    surname = input('Введите фамилию: ')
                     if surname:
-                        phone = input('\nВведите номер телефона: ')
+                        phone = input('Введите номер телефона: ')
                         if phone:
-                            elite = input('\nВведите избранный контакт: ')
-                            print('Добавление дополнительной информации.')
+                            elite = input('Введите избранный контакт: ')
+                            print('\nДобавление дополнительной информации.')
                             try:
-                                ph_nmbrs = input('\nВведите дополнительные тлф.номера через запятую: ')
+                                ph_nmbrs = input('Введите дополнительные тлф.номера через запятую: ') + ','
                                 list_ph_nmbrs = [i.strip() for i in ph_nmbrs.split(',')]
-                                adv_inf = input('\nВведите email и ссылки на соцсети через запятую: ')
+                                adv_inf = input('Введите email и ссылки на соцсети через запятую: ')
                                 dict_adv_inf = {str(k).strip(): str(v).strip() for [k, v] in
                                                 [i.split('=') for i in adv_inf.split(',')]}
                                 book_list[num].add_contact(name, surname, phone, elite,
@@ -102,39 +104,40 @@ def edit_book(num):
                                       'Неправильный формат ввода дополнительной информации.\n'
                                       'Пример: telegram=@jhony, email=jhony@smith.com')
             elif command == 'l':
-                print(book_list[num].list_contact())
+                book_list[num].list_contact()
             elif command == 'd':
                 phone = input('\nВведите номер телефона для удаления контакта: ')
                 book_list[num].dell_contact(phone)
             elif command == 's':
-                name = input('\nВведите имя контакта для поиска: ')
-                surname = input('\nВведите фамилию контакта для поиска: ')
+                name = input('Введите имя контакта для поиска: ')
+                surname = input('Введите фамилию контакта для поиска: ')
                 book_list[num].search_contact(name, surname)
         else:
             print('Введена недопустимая команда!')
 
 
-def print_header(title, text):
-    print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n{0:-^64}'.format(title))
+def print_header():
+    print('\n\n{0:-^64}'.format('Программа Телефонная Книга'))
     i = 0
     for i, item in enumerate(book_list):
         print(f'{i} - {item.name_phone_book}')
-    print('\n' * (8-i))
-    print(text)
+    print('\n' * (20-i))
+    print(f'n -новая тлф.книга  "номер" - открыть тлф.книгу  q -Выход')
 
 
 def main():
     while True:
-        print_header(title1, text_first)
+        print_header()
         command = input('\nВведите команду: ')
         if re.search(r'^[nq\d]?$', command) and command:
             if command =='q':
                 return
             elif command == 'n':
                 name = input('Введите имя новой телефонной книги: ')
-                book_list.append(PhoneBook(name)) if name else print('Вы ничего не ввели!')
+                book_list.append(PhoneBook(name)) if name else print('\nВы ничего не ввели!')
             else:
-                edit_book(int(command))
+                if int(command) < len(book_list):
+                    edit_book(int(command))
 
 
 if __name__ == '__main__':
