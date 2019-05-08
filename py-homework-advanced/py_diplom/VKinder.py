e39c74179b402c2dcae931c5452c278032dbd280
Aleksei Remnev <ran1024@yandex.ru>
2019-05-08 16:09:49 +1000

    Доделан ввод данных поиска от пользователя.

--- py-homework-advanced/py_diplom/VKinder.py
+++ py-homework-advanced/py_diplom/VKinder.py
@@ -53,28 +53,41 @@ class VkSession:
 
 def if_error(result):
     if result[0]:
-        print('Произошла ошибка авторизации. Попробуйте ещё раз.')
-        print(result[1])
+        print(f'Произошла ошибка авторизации: {result[1]}.\n Попробуйте ещё раз.')
         return 1
     else:
         return 0
 
 
-def get_find_param():
+def get_find_params(vkinder, vk_connect):
+    """
+    Получаем от пользователя необходимые параметры для поиска.
+    """
     print('Для поиска необходимо ввести несколько параметров.')
-    age_from = input('Введите минимальный возраст поиска: ')
-    age_to = input('Введите максимальный возраст поиска: ')
-    sex = input('Введите пол ("М". "Ж"): ')
-    sex = '1' if sex == 'М' else '2'
-    find_city = input('Введите город для поиска: ')
-    return [age_from, age_to, sex, find_city]
+    try:
+        age_min = int(input('Введите минимальный возраст поиска: '))
+        age_max = int(input('Введите максимальный возраст поиска: '))
+        sex_variant = {'м': 2, 'ж': 1}
+        sex = input('Введите пол ("М". "Ж"): ').lower()
+        sex = sex_variant[sex]
+    except (ValueError, KeyError):
+        return 1
+
+    city_title = input('Введите город для поиска: ')
+    if city_title:
+        result = vk_connect.vk.database.getCities(country_id=1, q=city_title, need_all=0, count=1)
+        find_city = result['items'][0]
+    else:
+        find_city = vkinder.city
+    interests = input('Введите желаемые интересы через запятую: ').split(',')
+
+    find_data = {'age_min': age_min, 'age_max': age_max, 'find_sex': sex,
+                 'find_city': find_city, 'find_interests': interests}
+    vkinder.update_find_params(find_data)
+    return 0
 
 
-def main():
-    connect = MongoClient()
-    db = connect.vkinder
-    vkinder = Vkinder(db)
-    vk_session = VkSession()
+def user_login(vkinder, vk_connect):
     text1 = '\nДля продолжения введите свой пароль в VK или Access Token, ' \
             'Рекомендуется получить Access Token. Для этого нужно перейти по ссылке:' \
             'https://oauth.vk.com/authorize?client_id=6888361&display=page' \
@@ -88,20 +101,33 @@ def main():
         login = input('Введите номер телефона: ')
         result = vkinder.find_vkinder(login)
         if not result[0] and result[1]:
-            res = vk_session.login_vk(token=result[1])
+            res = vk_connect.login_vk(token=result[1])
             session_ok = if_error(res)
         else:
             print(text1)
             password = input('Введите токен или пароль: ')
             if len(password) > 50:
                 vkinder.token = password
-                res = vk_session.login_vk(token=password)
+                res = vk_connect.login_vk(token=password)
             else:
-                res = vk_session.login_vk(login=login, password=password)
+                res = vk_connect.login_vk(login=login, password=password)
             session_ok = if_error(res)
         if not session_ok:
-            vkinder.update_user(login, vk_session.vk)
-            print('Это класс Vkinder:\n', vkinder)
+            vkinder.update_user(login, vk_connect.vk)
+
+
+def main():
+    connect = MongoClient()
+    db = connect.vkinder
+    vkinder = Vkinder(db)
+    vk_connect = VkSession()
+    user_login(vkinder, vk_connect)
+    result = 1
+    while result:
+        result = get_find_params(vkinder,vk_connect)
+        print('\nБыли введены неправильные параметры! Попробуйте ещё раз.\n')
+
+    print('\nЭто класс Vkinder:\n', vkinder)
 
     # account = vk.account.getProfileInfo()
     # vk_tools = vk_api.VkTools(vk)
--- py-homework-advanced/py_diplom/dbworker.py
+++ py-homework-advanced/py_diplom/dbworker.py
@@ -8,27 +8,6 @@
 from datetime import datetime
 
 
-# class UseDB:
-#     """
-#     Здесь собраны все средства для работы с базой данных.
-#     """
-#     def __init__(self):
-#         self.client = MongoClient()
-#         self.db = self.client.vkinder
-#         self.vkowners = self.db.vkowners
-#         self.vkusers = self.db.vkusers
-#
-#     def find_by_vkowners(self, index):
-#         result = self.vkowners.find_one(index, {'_id': 0})
-#         if result:
-#             return 0, result
-#         else:
-#             return 1, 'Not found.'
-#
-#     def update_vkowners(self, index, data):
-#         coll.update(index, data)
-
-
 class Vkinder:
 
     def __init__(self, db):
@@ -44,19 +23,26 @@ class Vkinder:
         self.age_min = 0
         self.age_max = 0
         self.find_sex = ''
-        self.find_city = ''
+        self.find_city = {}
         self.age_current = 0
         self.find_interests = []
         self.token = ''
+        self.login = ''
+        self.city = {}
 
     def __str__(self):
         return f'{{\n\tid: {self.id},\n\tfirst_name: {self.first_name}\n\tlast_name: {self.last_name}\n\t' \
-            f'screen_name: {self.screen_name}\n\tinterests: {self.interests}\n\tmusic: {self.music}\n\t' \
-            f'movies: {self.movies}\n\tbooks: {self.books}\n\tage_min: {self.age_min}\n\t' \
+            f'screen_name: {self.screen_name}\n\tlogin: {self.login}\n\tcity: {self.city}\n\t' \
+            f'interests: {self.interests}\n\tmusic: {self.music}\n\tmovies: {self.movies}\n\t' \
+            f'books: {self.books}\n\ttoken: {self.token}\n\tage_min: {self.age_min}\n\t' \
             f'age_max: {self.age_max}\n\tage_current: {self.age_current}\n\tfind_sex: {self.find_sex}\n\t' \
-            f'find_city: {self.find_city}\n\tfind_interests: {self.find_interests}\n\ttoken: {self.token}\n }}'
+            f'find_city: {self.find_city}\n\tfind_interests: {self.find_interests}\n }}'
 
     def find_vkinder(self, login):
+        """
+        Метод производит поиск пользователя в БД. Если пользователь уже зарегистрирован в приложении,
+        произходит заполнение атрибутов класса.
+        """
         result = self.vkinder.find_one({'login': login})
         if result:
             self.id = result['id']
@@ -67,32 +53,43 @@ class Vkinder:
             self.music = result['music']
             self.movies = result['movies']
             self.books = result['books']
-            self.age_min = result['age_min']
-            self.age_max = result['age_max']
-            self.find_sex = result['find_sex']
-            self.find_city = result['find_city']
-            self.age_current = result['age_current']
-            self.find_interests = result['find_interests']
             self.token = result['token']
+            self.login = result['login']
+            self.city = result['city']
+            if 'age_min' in result:
+                self.age_min = result['age_min']
+                self.age_max = result['age_max']
+                self.find_sex = result['find_sex']
+                self.find_city = result['find_city']
+                self.age_current = result['age_current']
+                self.find_interests = result['find_interests']
             return 0, self.token
         else:
             return 1, 'Пользователь не найден.'
 
     def update_user(self, login, vk):
-        result = vk.users.get(fields='interests, music, movies, books, screen_name')
+        """
+        Метод запрашивает данные пользователя в ВК (т.к. они могли измениться с момента
+        предыдущего запуска) и производит обновление записи в таблице базы данных vkinder.
+        """
+        result = vk.users.get(fields='interests, music, movies, books, screen_name, city')
         user = result[0]
+        user['interests'] = user['interests'].split(',')
         user.pop('is_closed', 0)
         user.pop('can_access_closed', 0)
         user['login'] = login
-        user['age_min'] = 0
-        user['age_max'] = 0
-        user['find_sex'] = ''
-        user['find_city'] = ''
-        user['age_current'] = 0
-        user['find_interests'] = []
         user['token'] = self.token
-        self.vkinder.update({'login': login}, user, upsert=True)
+        self.vkinder.update({'login': login}, {'$set': user}, upsert=True)
         self.find_vkinder(login)
+
+    def update_find_params(self, data):
+        """
+        Метод обновляет параметры поиска в базе данных и в экземпляре класса.
+        """
+        data['age_current'] = self.age_current
+        result = self.vkinder.update({'id': self.id}, {'$set': data}, upsert=False)
+        self.find_vkinder(self.login)
+        return result
 
 
 if __name__ == '__main__':
