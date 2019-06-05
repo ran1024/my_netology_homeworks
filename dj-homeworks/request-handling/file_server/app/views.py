5394de254ac53e004348df1c17ee0d96753ba644
Aleksei Remnev <ran1024@yandex.ru>
2019-06-05 12:26:06 +1000

    Переделан file_server.

--- dj-homeworks/request-handling/file_server/app/views.py
+++ dj-homeworks/request-handling/file_server/app/views.py
@@ -1,33 +1,34 @@
 import os
 from datetime import datetime
-
 from django.shortcuts import render
 from django.conf import settings
 
 
-def file_list(request, year=None, month=None, day=None):
+def file_list(request, date=None):
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
     template_name = 'index.html'
-    context = {'files': [], 'date': None}
+    context = {'files': [], 'date': date}
     arr = os.listdir(settings.FILES_PATH)
     arr.sort()
-    if year:
-        context['date'] = datetime(year, month, day).date()
+    if date:
+        dd = [int(x) for x in date.split('-')]
+        context['date'] = datetime(*dd).date()
     for fl in arr:
         stat_info = os.stat(f'{settings.FILES_PATH}/{fl}')
-        ctime = datetime.fromtimestamp(stat_info.st_ctime).date()
-        if context['date'] == ctime or not year:
-            context['files'].append({'name': fl,
-                'ctime': ctime,
-                'mtime': datetime.fromtimestamp(stat_info.st_mtime).date(),
+        c_time = datetime.fromtimestamp(stat_info.st_ctime)
+        m_time = datetime.fromtimestamp(stat_info.st_mtime)
+        if context['date'] == c_time.date() or context['date'] == m_time.date() or not date:
+            context['files'].append({
+                'name': fl,
+                'ctime': c_time,
+                'mtime': m_time,
             })      
-
     return render(request, template_name, context)
 
 
 def file_content(request, name):
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    context={'file_name': name, 'file_content': ''}
+    context = {'file_name': name, 'file_content': ''}
     with open(settings.FILES_PATH + '/' + name) as fh:
         context['file_content'] = fh.read()
     
